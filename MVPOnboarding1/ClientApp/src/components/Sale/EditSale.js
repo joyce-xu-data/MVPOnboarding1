06d561d483bf61d647a971cd8f8aa693244a4468
joyce-xu-data <joyce.xusuqiu@gmail.com>
2023-06-27 08:05:03 +0800

    updated edit sale, pop up not able to save but have a new window pop up and a dropdown list

--- MVPOnboarding1/ClientApp/src/components/Sale/EditSale.js
+++ MVPOnboarding1/ClientApp/src/components/Sale/EditSale.js
@@ -1,57 +1,195 @@
-﻿import React, { Component } from 'react';
+﻿import React, { useEffect, useState } from 'react';
 
-class EditSale extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            customerName: props.customerName,
-            productName: props.productName,
-            storeName: props.storeName,
-            dateSold: props.dateSold,
+const EditSale = (props) => {
+    const { saleId, customerName, productName, storeName, dateSold, onSave } = props;
+    const [editedCustomerName, setEditedCustomerName] = useState(customerName);
+    const [editedProductName, setEditedProductName] = useState(productName);
+    const [editedStoreName, setEditedStoreName] = useState(storeName);
+    const [editedDateSold, setEditedDateSold] = useState(dateSold);
+
+    const handleSave = () => {
+        onSave(saleId, editedCustomerName, editedProductName, editedStoreName, editedDateSold);
+        window.close();
+    };
+
+    useEffect(() => {
+        const editWindow = window.open('', '_blank', 'width=400,height=500');
+
+        const handlePopupMessage = (event) => {
+            const { type, customerName, productName, storeName, dateSold } = event.data;
+            if (type === 'updateSale') {
+                setEditedCustomerName(customerName);
+                setEditedProductName(productName);
+                setEditedStoreName(storeName);
+                setEditedDateSold(dateSold);
+            }
         };
-    }
 
-    handleInputChange = (event) => {
-        const { name, value } = event.target;
-        this.setState({ [name]: value });
-    };
+        editWindow.addEventListener('message', handlePopupMessage);
 
-    handleSave = () => {
-        const { saleId, onSave } = this.props;
-        const { customerName, productName, storeName, dateSold } = this.state;
-
-        // Perform save operation or API call here with updated data
-        // ...
-
-        onSave(saleId, { customerName, productName, storeName, dateSold });
-    };
-
-    render() {
-        const { customerName, productName, storeName, dateSold } = this.state;
-
-        return (
+        editWindow.document.write(`
+      <html>
+        <head>
+          <title>Edit Sale</title>
+          <style>
+            /* Additional custom styles for the edit popup window */
+            body {
+              font-family: 'Segoe UI', sans-serif;
+              background-color: #f9f9f9;
+              padding: 20px;
+            }
+              
+            h2 {
+              font-size: 24px;
+              margin-bottom: 20px;
+            }
+              
+            .form-container {
+              background-color: #fff;
+              border: 1px solid #ddd;
+              padding: 20px;
+            }
+              
+            .form-container select,
+            .form-container input {
+              width: 100%;
+              padding: 10px;
+              margin-bottom: 10px;
+            }
+              
+            .form-container button {
+              margin-right: 10px;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="form-container">
+            <h2>Edit Sale</h2>
             <div>
-                <h3>Edit Sale</h3>
-                <div>
-                    <label>Customer Name:</label>
-                    <input type="text" name="customerName" value={customerName} onChange={this.handleInputChange} />
-                </div>
-                <div>
-                    <label>Product Name:</label>
-                    <input type="text" name="productName" value={productName} onChange={this.handleInputChange} />
-                </div>
-                <div>
-                    <label>Store Name:</label>
-                    <input type="text" name="storeName" value={storeName} onChange={this.handleInputChange} />
-                </div>
-                <div>
-                    <label>Date Sold:</label>
-                    <input type="text" name="dateSold" value={dateSold} onChange={this.handleInputChange} />
-                </div>
-                <button onClick={this.handleSave}>Save</button>
+              <label>Customer:</label>
+              <select id="customerName">
+                <option value="">Select Customer</option>
+                ${props.sale.customers.map(
+            (customer) =>
+                `<option value="${customer.name}">${customer.name}</option>`
+        )}
+              </select>
             </div>
-        );
-    }
-}
+            <div>
+              <label>Product:</label>
+              <select id="productName">
+                <option value="">Select Product</option>
+                ${props.sale.products.map(
+            (product) =>
+                `<option value="${product.name}">${product.name}</option>`
+        )}
+              </select>
+            </div>
+            <div>
+              <label>Store:</label>
+              <select id="storeName">
+                <option value="">Select Store</option>
+                ${props.sale.stores.map(
+            (store) =>
+                `<option value="${store.name}">${store.name}</option>`
+        )}
+              </select>
+            </div>
+            <div>
+              <label>Date Sold:</label>
+              <input type="text" id="dateSold" placeholder="YYYY-MM-DD" />
+            </div>
+            <button id="saveButton">Save</button>
+            <button onclick="window.close()">Cancel</button>
+            <script>
+              const customerNameSelect = document.getElementById('customerName');
+              customerNameSelect.value = "${customerName}";
+              customerNameSelect.addEventListener('change', (event) => {
+                window.opener.postMessage(
+                  {
+                    type: 'updateSale',
+                    saleId: ${saleId},
+                    customerName: event.target.value,
+                    productName: document.getElementById('productName').value,
+                    storeName: document.getElementById('storeName').value,
+                    dateSold: document.getElementById('dateSold').value,
+                  },
+                  window.origin
+                );
+              });
+
+              const productNameSelect = document.getElementById('productName');
+              productNameSelect.value = "${productName}";
+              productNameSelect.addEventListener('change', (event) => {
+                window.opener.postMessage(
+                  {
+                    type: 'updateSale',
+                    saleId: ${saleId},
+                    customerName: document.getElementById('customerName').value,
+                    productName: event.target.value,
+                    storeName: document.getElementById('storeName').value,
+                    dateSold: document.getElementById('dateSold').value,
+                  },
+                  window.origin
+                );
+              });
+
+              const storeNameSelect = document.getElementById('storeName');
+              storeNameSelect.value = "${storeName}";
+              storeNameSelect.addEventListener('change', (event) => {
+                window.opener.postMessage(
+                  {
+                    type: 'updateSale',
+                    saleId: ${saleId},
+                    customerName: document.getElementById('customerName').value,
+                    productName: document.getElementById('productName').value,
+                    storeName: event.target.value,
+                    dateSold: document.getElementById('dateSold').value,
+                  },
+                  window.origin
+                );
+              });
+
+              const dateSoldInput = document.getElementById('dateSold');
+              dateSoldInput.value = "${dateSold}";
+              dateSoldInput.addEventListener('change', (event) => {
+                window.opener.postMessage(
+                  {
+                    type: 'updateSale',
+                    saleId: ${saleId},
+                    customerName: document.getElementById('customerName').value,
+                    productName: document.getElementById('productName').value,
+                    storeName: document.getElementById('storeName').value,
+                    dateSold: event.target.value,
+                  },
+                  window.origin
+                );
+              });
+
+              const saveButton = document.getElementById('saveButton');
+              saveButton.addEventListener('click', () => {
+                ${handleSave.toString()}();
+              });
+
+              // Remove the event listener when the pop-up window is closed
+              window.addEventListener('beforeunload', () => {
+                saveButton.removeEventListener('click', () => {
+                  ${handleSave.toString()}();
+                });
+              });
+            </script>
+          </div>
+        </body>
+      </html>
+    `);
+
+        return () => {
+            editWindow.removeEventListener('message', handlePopupMessage);
+            editWindow.close();
+        };
+    }, [customerName, productName, storeName, dateSold, saleId, onSave, props.sale.customers, props.sale.products, props.sale.stores]);
+
+    return null; // Since this is a popup window, return null as we don't need to render anything
+};
 
 export default EditSale;
--- MVPOnboarding1/ClientApp/src/components/Sale/SaleList.js
+++ MVPOnboarding1/ClientApp/src/components/Sale/SaleList.js
@@ -35,157 +35,10 @@ export class SaleList extends Component {
         this.setState({ showCreatePopup: false });
     };
 
-    handleSave = (saleId, updatedSaleData) => {
-        const { sales } = this.state;
-
-        // Find the index of the sale with the specified saleId
-        const saleIndex = sales.findIndex(sale => sale.id === saleId);
-
-        if (saleIndex !== -1) {
-            // Create a copy of the sales array
-            const updatedSales = [...sales];
-
-            // Update the sale data at the specified index
-            updatedSales[saleIndex] = {
-                ...updatedSales[saleIndex],
-                ...updatedSaleData
-            };
-
-            // Update the state with the updated sales data
-            this.setState({ sales: updatedSales });
-        }
-
-        // Close the edit form
-        this.setState({ editingSaleId: null });
-    };
-
-
-    handleEdit = (saleId, customerName, productName, storeName, dateSold) => {
-        this.setState({
-            editingSaleId: saleId,
-            editedCustomerName: customerName,
-            editedProductName: productName,
-            editedStoreName: storeName,
-            editedDateSold: dateSold,
-        });
-    };
-
     componentDidMount() {
         this.populateSaleData();
         window.addEventListener('message', this.handlePopupMessage);
     }
-
-    componentWillUnmount() {
-        window.removeEventListener('message', this.handlePopupMessage);
-    }
-
-
-    async populateSaleData() {
-        try {
-            const response = await fetch('api/sales');
-            const data = await response.json();
-            this.setState({ sales: data, loading: false });
-
-            const customerResponse = await fetch('api/customers');
-            const productResponse = await fetch('api/products');
-            const storeResponse = await fetch('api/stores');
-
-            const customerData = await customerResponse.json();
-            const productData = await productResponse.json();
-            const storeData = await storeResponse.json();
-
-            this.setState({
-                customers: customerData,
-                products: productData,
-                stores: storeData,
-            });
-        } catch (error) {
-            this.setState({ error: error.message });
-        }
-    }
-
-    handlePopupMessage = (event) => {
-        const { type, customerName, productName, storeName, dateSold } = event.data;
-        if (type === 'createSale') {
-            const newSale = {
-                id: this.state.sales.length + 1,
-                customerName,
-                productName,
-                storeName,
-                dateSold,
-            };
-            this.handleCreateCustomer(newSale);
-        } else if (type === 'cancelCreateSale') {
-            this.closeCreatePopup();
-        }
-    };
-    handleCreateCustomer = async (saleData) => {
-        // Make an API request to create the customer
-        try {
-            const response = await fetch('api/sales', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(saleData),
-            });
-
-            if (response.ok) {
-                console.log('New sale created.');
-                this.setState({ showCreatePopup: false });
-                this.populateSaleData();
-            } else {
-                const errorData = await response.json();
-                const errorMessage = errorData.message || 'Failed to create sale.';
-                throw new Error(errorMessage);
-            }
-        } catch (error) {
-            console.error('Error:', error.message);
-            this.setState({ error: error.message });
-        }
-    };
-
-
-    handleDelete = async (saleId) => {
-        // Open a new window
-        const deleteWindow = window.open('', '_blank', 'width=400,height=300');
-
-        // Generate the content for the new window using the separate function
-        const deleteWindowContent = generateDeleteWindowContent(saleId);
-
-        // Write the content of the new window
-        deleteWindow.document.write(deleteWindowContent);
-
-        // Global function to handle confirmation of deletion in the main window
-        window.confirmDeleteSale = (saleId) => {
-            this.handleConfirmDelete(saleId);
-            window.close();
-        };
-    };
-
-    handleConfirmDelete = async (saleId) => {
-        // Make an API request to delete the sale
-        try {
-            const response = await fetch(`api/sales/${saleId}`, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-
-            if (response.ok) {
-                console.log(`Sale with ID ${saleId} deleted.`);
-                this.populateSaleData();
-            } else {
-                const errorData = await response.json();
-                const errorMessage = errorData.message || 'Failed to delete sale.';
-                throw new Error(errorMessage);
-            }
-        } catch (error) {
-            console.error('Error:', error.message);
-            this.setState({ error: error.message });
-        }
-    };
 
     render() {
         const {
@@ -239,16 +92,206 @@ export class SaleList extends Component {
 
                 {editingSaleId && (
                     <EditSale
+                        sale={{
+                            customers,
+                            products,
+                            stores,
+                        }}
                         saleId={editingSaleId}
-                        customerName={editedCustomerName}
-                        productName={editedProductName}
-                        storeName={editedStoreName}
-                        dateSold={editedDateSold}
-                        onSave={this.handleSave} // Pass the onSave prop correctly
+                        customerName={this.state.editedCustomerName}
+                        productName={this.state.editedProductName}
+                        storeName={this.state.editedStoreName}
+                        dateSold={this.state.editedDateSold}
+                        onSave={this.handleSave}
                     />
+
                 )}
             </div>
         );
+    }
+
+    handlePopupMessage = (event) => {
+        const { type, customerName, productName, storeName, dateSold } = event.data;
+        if (type === 'createSale') {
+            const newSale = {
+                id: this.state.sales.length + 1,
+                customerName,
+                productName,
+                storeName,
+                dateSold,
+            };
+            this.handleCreateCustomer(newSale);
+        } else if (type === 'cancelCreateSale') {
+            this.closeCreatePopup();
+        }
+    };
+
+    handleCreateCustomer = async (saleData) => {
+        // Make an API request to create the customer
+        try {
+            const response = await fetch('api/sales', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(saleData),
+            });
+
+            if (response.ok) {
+                console.log('New sale created.');
+                this.setState({ showCreatePopup: false });
+                this.populateSaleData();
+            } else {
+                const errorData = await response.json();
+                const errorMessage = errorData.message || 'Failed to create sale.';
+                throw new Error(errorMessage);
+            }
+        } catch (error) {
+            console.error('Error:', error.message);
+            this.setState({ error: error.message });
+        }
+    };
+
+    handleEdit = (saleId, customerName, productName, storeName, dateSold) => {
+        this.setState({
+            editingSaleId: saleId,
+            editedCustomerName: customerName,
+            editedProductName: productName,
+            editedStoreName: storeName,
+            editedDateSold: dateSold,
+        });
+
+        // Global 
+        window.updateEditedCustomerName = (value) => {
+            this.setState({ editedCustomerName: value });
+        };
+
+        window.updateEditedProductName = (value) => {
+            this.setState({ editedProductName: value });
+        };
+
+        window.updateEditedStoreName = (value) => {
+            this.setState({ editedStoreName: value });
+        };
+
+        window.updateEditedDateSold = (value) => {
+            this.setState({ editedDateSold: value });
+        };
+
+        window.saveEditeDateSold = (saleId) => {
+            this.handleSave(saleId);
+        };
+    };
+
+    handleDelete = async (saleId) => {
+        // Open a new window
+        const deleteWindow = window.open('', '_blank', 'width=400,height=300');
+
+        // Generate the content for the new window using the separate function
+        const deleteWindowContent = generateDeleteWindowContent(saleId);
+
+        // Write the content of the new window
+        deleteWindow.document.write(deleteWindowContent);
+
+        // Global function to handle confirmation of deletion in the main window
+        window.confirmDeleteSale = (saleId) => {
+            this.handleConfirmDelete(saleId);
+            window.close();
+        };
+    };
+
+    handleConfirmDelete = async (saleId) => {
+        // Make an API request to delete the sale
+        try {
+            const response = await fetch(`api/sales/${saleId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.ok) {
+                console.log(`Sale with ID ${saleId} deleted.`);
+                this.populateSaleData();
+            } else {
+                const errorData = await response.json();
+                const errorMessage = errorData.message || 'Failed to delete sale.';
+                throw new Error(errorMessage);
+            }
+        } catch (error) {
+            console.error('Error:', error.message);
+            this.setState({ error: error.message });
+        }
+    };
+
+    handleSave = async (saleId) => {
+        const {
+            editedCustomerName,
+            editedProductName,
+            editedStoreName,
+            editedDateSold,
+        } = this.state;
+
+        // Make an API request to update the sale with the edited values
+        try {
+            const response = await fetch(`api/sales/${saleId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id: saleId,
+                    customerName: editedCustomerName,
+                    productName: editedProductName,
+                    storeName: editedStoreName,
+                    dateSold: editedDateSold,
+                }),
+            });
+
+            if (response.ok) {
+                console.log(`Sale with ID ${saleId} updated.`);
+                this.populateSaleData();
+                // You may want to update the state or refresh the sale list
+            } else {
+                const errorData = await response.json();
+                const errorMessage = errorData.message || 'Failed to update sale.';
+                throw new Error(errorMessage);
+            }
+        } catch (error) {
+            console.error('Error:', error.message);
+            this.setState({ error: error.message });
+        }
+
+        this.handleCancelEdit();
+    };
+
+
+    handleCancelEdit = () => {
+        this.setState({ editingSaleId: null, editedCustomerName: '', editedProductName: '', editedStoreName: '', editedDateSold: '' });
+    };
+
+    async populateSaleData() {
+        try {
+            const response = await fetch('api/sales');
+            const data = await response.json();
+            this.setState({ sales: data, loading: false });
+
+            const customerResponse = await fetch('api/customers');
+            const productResponse = await fetch('api/products');
+            const storeResponse = await fetch('api/stores');
+
+            const customerData = await customerResponse.json();
+            const productData = await productResponse.json();
+            const storeData = await storeResponse.json();
+
+            this.setState({
+                customers: customerData,
+                products: productData,
+                stores: storeData,
+            });
+        } catch (error) {
+            this.setState({ error: error.message });
+        }
     }
 }
 
